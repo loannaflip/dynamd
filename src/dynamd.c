1c658b98b788c9120626c8edacaea1087502c29d
uniminin <uniminin@zoho.com>
2021-03-22 13:06:43 +0600

    add status bar on all monitors

--- src/dynamd.c
+++ src/dynamd.c
@@ -1666,7 +1666,7 @@ void drawbar(Monitor *m) {
     Client *c;
 
     /* draw status first so it can be overdrawn by tags later */
-    if (m == selmon) { /* status is only drawn on selected monitor */
+    if (m == selmon || 1) { /* status is only drawn on selected monitor */
         drw_setscheme(drw, scheme[SchemeNorm]);
         sw = TEXTW(stext) - lrpad + 2; /* 2px right padding */
         drw_text(drw, m -> ww - sw, 0, sw, bh, 0, stext, 0);
@@ -3319,11 +3319,15 @@ void updatesizehints(Client *c) {
 
 
 void updatestatus() {
+    Monitor* m;
+
     if (!gettextprop(root, XA_WM_NAME, stext, sizeof(stext))) {
         strcpy(stext, "dynamd");
     }
 
-    drawbar(selmon);
+    for (m = mons; m; m = m -> next) {
+        drawbar(m);
+    }
 }
 
 
