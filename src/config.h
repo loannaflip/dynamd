9a46160062d9fe202fef6a3c71735aff5ef3f127
uniminin <uniminin@zoho.com>
2021-03-26 11:07:10 +0600

    change script location

--- src/config.h
+++ src/config.h
@@ -60,7 +60,7 @@ static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9",
 
 /* Startup Script */
 static const char *const autostart[] = {
-    "sh", "-c", "/usr/src/dynamd/startup/startup.sh", NULL,
+    "sh", "-c", "~/startup/startup.sh", NULL,
     NULL
 };
 
